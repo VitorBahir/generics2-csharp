12e293be99f07d42110342cded102a895cf2ab5b
Vitor Bahir <vitorb82014@gmail.com>
2023-03-27 00:53:33 -0300

    Initial implementation

---
+++ Course/Services/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Course.Services
+{
+    class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                // if result is 1, list is bigger. If result is -1, max is bigger. If result is 0, max = list
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
